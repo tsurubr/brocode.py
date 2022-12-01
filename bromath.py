341c1b319810884de571b86d094dddd2c8967987
tsurubr <protsuru@gmail.com>
2022-12-01 07:58:51 -0300

    math repository understanding

---
+++ bromath.py
@@ -0,0 +1,15 @@
+import math
+
+pi = 3.14
+x = 1
+y = 5.4
+z = 54
+
+# print(round(pi))
+# print(math.cei(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi, 2))
+# print(math.sqrt(pi))
+# print(max(pi, x , y ,z))
+# print(min(x, y, z, pi))
