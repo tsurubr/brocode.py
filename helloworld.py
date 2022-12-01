dfa09cd86c2a23aab8b9d0f68bbaf19309839f9b
tsurubr <protsuru@gmail.com>
2022-12-01 07:45:28 -0300

    bro

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+name = input("Qual o seu nome?: ")
+age = int(input("Qual a sua idade?: "))
+height = float(input("Qual a sua altura?: "))
+
+print("Olá " + name)
+print(("Você tem " + str(age) + " anos")) 
+print("Você tem " + str(height) + "m de altura")
